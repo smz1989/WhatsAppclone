ccfbdc4fdcb0ce727d8dff7668444bfb8bd9dcdf
smz1989 <sahandmohammadiziabari@gmail.com>
2019-03-06 11:34:23 +0100

    Adding Social Media Activity when somebody logged in!

--- app/src/main/java/com/example/whatsappclone/LoginActivity.java
+++ app/src/main/java/com/example/whatsappclone/LoginActivity.java
@@ -1,13 +1,118 @@
 package com.example.whatsappclone;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edtLoginEmail, edtLoginPassword;
+    private Button btnLoginActivity, btnSignUpLoginActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setTitle("Log In");
+
+        edtLoginEmail = findViewById(R.id.edtLoginEmail);
+        edtLoginPassword = findViewById(R.id.edtLoginPassword);
+        edtLoginPassword.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+
+                    onClick(btnLoginActivity);}
+
+                return false;
+            }
+        });
+
+
+        btnLoginActivity = findViewById(R.id.btnLoginActivity);
+        btnSignUpLoginActivity = findViewById(R.id.btnSignUpLoginActivity);
+
+        btnLoginActivity.setOnClickListener(LoginActivity.this);
+        btnSignUpLoginActivity.setOnClickListener(LoginActivity.this);
+
+
+
+
+        if (ParseUser.getCurrentUser() != null) {
+            transitionToWhatsAppUsersActivity();
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.btnLoginActivity:
+
+
+                if (edtLoginEmail.getText().toString().equals("") ||
+                        edtLoginPassword.getText().toString().equals("")){
+
+                    FancyToast.makeText(LoginActivity.this, "Email, Username, Password is required " ,Toast.LENGTH_SHORT,FancyToast.INFO,true).show();
+
+
+                }else {
+
+                    final ProgressDialog progressDialog = new ProgressDialog(this);
+                    progressDialog.setMessage("Logging Up" + edtLoginEmail.getText().toString());
+                    progressDialog.show();
+
+                    ParseUser.logInInBackground(edtLoginEmail.getText().toString(), edtLoginPassword.getText().toString(),
+                            new LogInCallback() {
+                                @Override
+                                public void done(ParseUser user, ParseException e) {
+
+                                    if (user != null && e == null) {
+
+                                        FancyToast.makeText(LoginActivity.this, user.getUsername() + " is logged in successfully", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+
+                                        transitionToWhatsAppUsersActivity();
+
+                                    }
+
+                                    progressDialog.dismiss();
+                                }
+                            });
+                }
+
+                break;
+
+            case R.id.btnSignUpLoginActivity:
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                break;
+
+        }
+
+
+    }
+
+    private void transitionToWhatsAppUsersActivity(){
+
+        Intent intent = new Intent(LoginActivity.this, WhatsAppUsersActivity.class);
+        startActivity(intent);
+
     }
 }
